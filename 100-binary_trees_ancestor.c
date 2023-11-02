4cf64c3dd7c744ef761928fae09c1280fa7f37cd
JozeSIMAO <jozesimao10@gmail.com>
2023-11-02 22:48:34 +0200

    100-betty fix

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -4,28 +4,35 @@
  * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
  * @first: Pointer to the first node.
  * @second: Pointer to the second node.
- * Return: Pointer to the lowest common ancestor node or NULL if no common ancestor was found.
+ * Return: Pointer to the lowest common ancestor node
+ * or NULL if no common ancestor was found.
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
-    if (first == NULL || second == NULL)
-        return (NULL);
+	const binary_tree__t *first_parent, *second_parent;
+	binary_tree_t *lowest_common_ancestor;
 
-    if (first == second)
-        return ((binary_tree_t *)first);
+	if (first == NULL || second == NULL)
+		return (NULL);
 
-    binary_tree_t *lowest_common_ancestor = NULL;
-    const binary_tree_t *first_parent = first;
-    while (first_parent != NULL)
-    {
-        const binary_tree_t *second_parent = second;
-        while (second_parent != NULL)
-        {
-            if (first_parent == second_parent)
-                lowest_common_ancestor = (binary_tree_t *)first_parent;
-            second_parent = second_parent->parent;
-        }
-        first_parent = first_parent->parent;
-    }
-    return lowest_common_ancestor;
+	if (first == second)
+		return ((binary_tree_t *)first);
+
+	lowest_common_ancestor = NULL;
+	first_parent = first;
+
+	while (first_parent != NULL)
+	{
+		second_parent = second;
+		while (second_parent != NULL)
+		{
+			if (first_parent == second_parent)
+				lowest_common_ancestor = (binary_tree_t *)first_parent;
+			second_parent = second_parent->parent;
+		}
+		first_parent = first_parent->parent;
+	}
+
+	return (lowest_common_ancestor);
 }
